91114b485d8b6084cf264fcdeff61f5e31783641
trieu123x <trieudh14@gmail.com>
2026-04-26 18:01:03 +0700

    fix email

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "[IP_ADDRESS]", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
