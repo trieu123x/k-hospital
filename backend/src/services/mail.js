7d2c96da33a1f8d00fa8ea1476f45e0500b82a5f
trieu123x <trieudh14@gmail.com>
2026-04-26 22:59:29 +0700

    switch port 587

--- backend/src/services/mail.js
+++ backend/src/services/mail.js
@@ -4,15 +4,16 @@ import dns from "dns";
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000
-});;
+  connectionTimeout: 10000,
+});
 
 transporter.verify((error) => {
     if (error) {
